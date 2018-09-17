f66d68c71deeb4546edef4d87a0fa5bc42f975ba
Travis Schnider <travis.schnider@gmail.com>
2018-09-17 00:43:11 -0600

    Create basic registration page and login test.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-FacebookTest
+* Dependencies
+
+    * Geckodriver 
+        
+        * (I placed my executable in `/usr/local/bin`), with this approach Selenium automatically finds it from your path.
+        * Alternatively you can set it in the code like so: `System.setProperty("webdriver.gecko.driver", "/path/to/geckodriver");
+    
+    * Set two environment variables for the email and password that you want to use to login.
+    
---
+++ Tests/src/TestRegistrationPageLogin.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestRegistrationPageLogin
+{
+
+    public static void main(String[] args)
+    {
+        String emailAddress = System.getenv("fbemail");
+        String password = System.getenv("fbpass");
+        WebDriver driver = new FirefoxDriver();
+
+        driver.navigate().to("http://www.facebook.com");
+
+        RegistrationPage registrationPage = new RegistrationPage(driver);
+
+        registrationPage.setEmail(emailAddress);
+        registrationPage.setPass(password);
+        registrationPage.clickSubmit();
+
+        driver.quit();
+
+    }
+}
